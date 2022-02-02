a5cd86e3b0ca95f58ce6d9dee2ee111e00bba47f
qaacademy <qaacademybr@gmail.com>
2022-02-01 21:18:48 -0300

    Transformação Exercicio 02 Easy - metodo com Retorno e com passagem de parametros

---
+++ src/easy/easy/Execucao.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class Execucao {
+
+    public static void main(String[] args) {
+        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        String palavra = exercicio02_easy.retornaPalavra("Flavio Dias");
+        System.out.println(palavra);
+    }
+    
+}
--- src/easy/easy/Exercicio02_easy.java
+++ src/easy/easy/Exercicio02_easy.java
@@ -1,11 +1,10 @@
 package easy;
+
 import javax.swing.JOptionPane;
 
-public class Exercicio02_easy {//incio da classe Exercicio
-   
-public static void main(String[] args) {
-    String palavraDigitada; 
-	palavraDigitada  = JOptionPane.showInputDialog("Digite uma palavra!"); 
-	System.out.println("O usuario digitou : "+palavraDigitada) ; 
-}
-}//fim exercicio
+public class Exercicio02_easy {// incio da classe Exercicio
+
+	public String retornaPalavra(String palavra) {
+		return "O usuario digitou : " + palavra;
+	}
+}// fim exercicio
