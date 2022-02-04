f7a8648c82f620094ee1ea4cdf620f39bb5f1650
qaacademy <qaacademybr@gmail.com>
2022-02-03 22:07:17 -0300

    Refatoração Exercicio 08 - Easy

--- src/easy/easy/Execucao.java
+++ src/easy/easy/Execucao.java
@@ -18,31 +18,40 @@ public class Execucao {
         // segundoValor);
         // System.out.println(valorTrocado); //Faço a chamada e valor
 
-        // String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um numero");
+        // String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um
+        // numero");
         // int numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
         // Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
-        // System.out.println("O dobro de "+ numeroDigitado +" = " +exercicio04_easy.calcularDobro(numeroDigitado));
+        // System.out.println("O dobro de "+ numeroDigitado +" = "
+        // +exercicio04_easy.calcularDobro(numeroDigitado));
 
-
-        // String primeiroDigitadoStg = JOptionPane.showInputDialog("Digite o primeiro número");
-        // String segundoDigitadoStg = JOptionPane.showInputDialog("Digite o segundo número");
-        // String terceiroDigitadoStg = JOptionPane.showInputDialog("Digite o terceiro número");
+        // String primeiroDigitadoStg = JOptionPane.showInputDialog("Digite o primeiro
+        // número");
+        // String segundoDigitadoStg = JOptionPane.showInputDialog("Digite o segundo
+        // número");
+        // String terceiroDigitadoStg = JOptionPane.showInputDialog("Digite o terceiro
+        // número");
 
         // int primeiroNumero = Integer.parseInt(primeiroDigitadoStg);
         // int segundoNumero = Integer.parseInt(segundoDigitadoStg);
         // int terceiroNumero = Integer.parseInt(terceiroDigitadoStg);
 
         // Exercicio05_easy exercicio05_easy = new Exercicio05_easy();
-        // System.out.println("A Soma dos numeros digitados será: "+exercicio05_easy.soma(primeiroNumero, segundoNumero, terceiroNumero));
-        // System.out.println("A Subtração dos numeros digitados será: "+exercicio05_easy.subtracao(primeiroNumero, segundoNumero, terceiroNumero));
-        // System.out.println("A Multiplicação dos numeros digitados será: "+exercicio05_easy.multiplicacao(primeiroNumero, segundoNumero, terceiroNumero));
-        // int soma = exercicio05_easy.soma(primeiroNumero, segundoNumero, terceiroNumero);
-        // System.out.println("A Média dos numeros digitados será: "+exercicio05_easy.media(soma));
-
-
+        // System.out.println("A Soma dos numeros digitados será:
+        // "+exercicio05_easy.soma(primeiroNumero, segundoNumero, terceiroNumero));
+        // System.out.println("A Subtração dos numeros digitados será:
+        // "+exercicio05_easy.subtracao(primeiroNumero, segundoNumero, terceiroNumero));
+        // System.out.println("A Multiplicação dos numeros digitados será:
+        // "+exercicio05_easy.multiplicacao(primeiroNumero, segundoNumero,
+        // terceiroNumero));
+        // int soma = exercicio05_easy.soma(primeiroNumero, segundoNumero,
+        // terceiroNumero);
+        // System.out.println("A Média dos numeros digitados será:
+        // "+exercicio05_easy.media(soma));
 
         // String nota1Stg, nota2Stg;
-        // nota1Stg = JOptionPane.showInputDialog("Por favor, digite a primeira nota: ");
+        // nota1Stg = JOptionPane.showInputDialog("Por favor, digite a primeira nota:
+        // ");
         // nota2Stg = JOptionPane.showInputDialog("Por favor, digite a segunda nota: ");
 
         // double nota1 = Double.parseDouble(nota1Stg);
@@ -51,11 +60,21 @@ public class Execucao {
         // Exercicio06_easy exercicio06_easy = new Exercicio06_easy();
         // System.out.println(exercicio06_easy.verificaAprovacao(nota1, nota2));
 
-        String salarioStg = JOptionPane.showInputDialog("Por favor, digite um valor de salario: "); // Leitura da digitação
-        double salario = Double.parseDouble(salarioStg);// Conversão e atribuição na variavel salario
-        Exercicio07_easy exercicio07_easy = new Exercicio07_easy();
-        System.out.println("O valor do inss para um salario de : "+ salario +"Será de : "+ exercicio07_easy.calculaInss(salario));
-    
+        // String salarioStg = JOptionPane.showInputDialog("Por favor, digite um valor
+        // de salario: "); // Leitura da digitação
+        // double salario = Double.parseDouble(salarioStg);// Conversão e atribuição na
+        // variavel salario
+        // Exercicio07_easy exercicio07_easy = new Exercicio07_easy();
+        // System.out.println("O valor do inss para um salario de : "+ salario +"Será de
+        // : "+ exercicio07_easy.calculaInss(salario));
+
+        String salarioStg = JOptionPane.showInputDialog("Por favor, digite um valor de salario: ");
+        double salario = Double.parseDouble(salarioStg);
+
+        Exercicio08_easy exercicio08_easy = new Exercicio08_easy();
+        System.out.println("O Imposto de renda será: " + exercicio08_easy.calculaImpostoDeRenda(salario));
+        double impostoDeRenda = exercicio08_easy.calculaImpostoDeRenda(salario);
+        System.out.println("O Salario líquido será: " + exercicio08_easy.calculaSalarioLiquido(salario, impostoDeRenda));
     }
 
 }
