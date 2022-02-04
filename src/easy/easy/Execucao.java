d66a985107a2e5a04d208c1a9bb295930af9ea87
qaacademy <qaacademybr@gmail.com>
2022-02-03 21:14:40 -0300

    Refatoração Exercicio 04 - Easy

--- src/easy/easy/Execucao.java
+++ src/easy/easy/Execucao.java
@@ -9,11 +9,20 @@ public class Execucao {
         // String palavra = exercicio02_easy.retornaPalavra("Flavio Dias");
         // System.out.println(palavra);
 
-        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
-        String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
-        Exercicio03_easy exercicio03_easy = new Exercicio03_easy();// Instanciando a classe Exercicio03_easy 
-        String valorTrocado = exercicio03_easy.retornaValorTrocado(primeiroValor, segundoValor);
-        System.out.println(valorTrocado); //Faço a chamada e valor
+        // String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro
+        // valor");
+        // String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
+        // Exercicio03_easy exercicio03_easy = new Exercicio03_easy();// Instanciando a
+        // classe Exercicio03_easy
+        // String valorTrocado = exercicio03_easy.retornaValorTrocado(primeiroValor,
+        // segundoValor);
+        // System.out.println(valorTrocado); //Faço a chamada e valor
+
+        String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um numero");
+        int numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+        System.out.println("O dobro de "+ numeroDigitado +" = " +exercicio04_easy.calcularDobro(numeroDigitado));
+
     }
-    
+
 }
--- src/easy/easy/Exercicio04_easy.java
+++ src/easy/easy/Exercicio04_easy.java
@@ -1,17 +1,12 @@
 package easy;
+
 import javax.swing.JOptionPane;
 
 public class Exercicio04_easy {
 
-    public static void main(String[] args) {
-        int numeroDigitado, resultado;
-        String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um numero");
-        numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+    public int calcularDobro(int numeroDigitado) {
+        int resultado;
         resultado = numeroDigitado * 2;
-        System.out.println("O dobro do número digitado é: " + resultado);// Console
-        JOptionPane.showMessageDialog(null,"O dobro do número digitado é: " + resultado); //Mostra numa caixa de texto
-
+        return resultado;
     }
-
-
 }
